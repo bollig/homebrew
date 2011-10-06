166496ab7e2fcf3220db8327fae6c9e5cff8863a
Evan Bollig <bollig@scs.fsu.edu>
2011-10-06 14:28:15 -0600

    Modifications of armadillo formula following suggestions from @jacknagel.

--- Library/Formula/armadillo.rb
+++ Library/Formula/armadillo.rb
@@ -9,18 +9,8 @@ class Armadillo < Formula
   depends_on 'boost'
 
   def install
-    #system "./configure", "--disable-debug", "--disable-dependency-tracking",
-    #                      "--prefix=#{prefix}"
     system "cmake . #{std_cmake_parameters}"
     system "make install"
   end
 
-  def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test armadillo`. Remove this comment before submitting
-    # your pull request!
-    system "false"
-  end
 end
